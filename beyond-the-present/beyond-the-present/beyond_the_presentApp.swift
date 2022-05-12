2ee38b32cfef0dbd89b95b637822d320c83bcb10
fuziki <hymne.shannon.projects@gmail.com>
2022-05-12 20:48:01 +0900

    create init project

---
+++ beyond-the-present/beyond-the-present/beyond_the_presentApp.swift
@@ -0,0 +1,17 @@
+//
+//  beyond_the_presentApp.swift
+//  beyond-the-present
+//
+//  Created by fuziki on 2022/05/12.
+//
+
+import SwiftUI
+
+@main
+struct beyond_the_presentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
